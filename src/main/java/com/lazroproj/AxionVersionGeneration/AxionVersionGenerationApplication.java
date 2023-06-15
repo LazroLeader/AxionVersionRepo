c3f030cac706f572da3a2c290e383a7ac284a6bb
LazroLeader <l.giorgobiani@lider-bet.com>
2023-06-15 11:28:01 +0400

    Added Some development

--- src/main/java/com/lazroproj/AxionVersionGeneration/AxionVersionGenerationApplication.java
+++ src/main/java/com/lazroproj/AxionVersionGeneration/AxionVersionGenerationApplication.java
@@ -8,6 +8,7 @@ public class AxionVersionGenerationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AxionVersionGenerationApplication.class, args);
+		// Added Some development
 	}
 
 }
