a068c0ca08f19623fccfb8cbce55228b0b3596b9
LazroLeader <l.giorgobiani@lider-bet.com>
2023-06-15 11:26:10 +0400

    Initial Commit

---
+++ src/main/java/com/lazroproj/AxionVersionGeneration/AxionVersionGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.lazroproj.AxionVersionGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxionVersionGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxionVersionGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazroproj/AxionVersionGeneration/AxionVersionGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazroproj.AxionVersionGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxionVersionGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
